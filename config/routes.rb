ba6682db3ffb940cea4f957faa5d47c51a18e3f0
Laurice Minamihara <lauricejoie.m@gmail.com>
2024-01-11 20:05:26 +0900

    index and show

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   resources :restaurants do
-    resources :reviews, only: [:destroy]
+    resources :reviews, only: %i[new create]
   end
 end
